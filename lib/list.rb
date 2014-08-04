a78ca17878f477e3806e6c636e8eb857572f43ff
Austin Steward & Sean Morrison <student@epicodus.com>
2014-08-04 14:28:33 -0700

    :oncoming_bus: Add database support

---
+++ lib/list.rb
@@ -0,0 +1,27 @@
+require 'pg'
+
+class List
+  attr_accessor :name
+
+  def initialize (name)
+    @name = name
+  end
+
+  def ==(another_task)
+    self.name == another_task.name
+  end
+
+  def self.all
+    results = DB.exec("SELECT * FROM lists;")
+    lists = []
+    results.each do |result|
+      name = result['name']
+      lists << List.new(name)
+    end
+    lists
+  end
+
+  def save
+    DB.exec("INSERT INTO lists (name) VALUES ('#{@name}');")
+  end
+end
